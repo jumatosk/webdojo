74f060d163ac56253d126b68a4241f6fac1432ff
Juliana Matos <jumatosk@gmail.com>
2025-10-16 10:27:58 -0300

    feat: interagindo com botão de radio

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -5,8 +5,8 @@ describe("Formulário de consultoria", () => {
 
     cy.goTo("Formulários", "Consultoria");
 
-    // cy.get("#name").type("Juliana Matos");
-    // cy.get("#email").type("juliana@webdojo");
+    cy.get("#name").type("Juliana Matos");
+    cy.get("#email").type("juliana@webdojo");
 
     cy.get("input[placeholder='Digite seu nome completo']").type(
       "Juliana Matos"
@@ -17,12 +17,22 @@ describe("Formulário de consultoria", () => {
       .should("have.value", "(71) 98888-4545");
 
     //Estratégia utilizada para encontrar campo através do id
-    // cy.get("#consultancyType").select("In Company");
+    cy.get("#consultancyType").select("In Company");
 
     // Estratégia utilizada para encontrar campo através da label e do seu elemento pai
     cy.contains("label", "Tipo de Consultoria")
       .parent()
       .find("select")
       .select("In Company");
+
+    cy.contains("span", "Pessoa Física")
+      .parent()
+      .find("input")
+      .check()
+      .should("be.checked");
+
+    cy.contains("label", "Pessoa Jurídica")
+      .find("input")
+      .should("be.not.checked");
   });
 });

616a353e257332a00c636fbdaea042c4b06258dd
Juliana Matos <jumatosk@gmail.com>
2025-10-21 11:31:08 -0300

    feat: cypress-real-events simulando evento de mouseover

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -23,6 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import "cypress-real-events";
+
 Cypress.Commands.add("start", () => {
   cy.viewport(1200, 840);
   cy.visit("/");

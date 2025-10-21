da806200da0160340997d64c7689c17ae45fef8a
Juliana Matos <jumatosk@gmail.com>
2025-10-21 15:37:49 -0300

    feat: testando links que abrem em nova aba

---
+++ web/cypress/e2e/links.cy.js
@@ -0,0 +1,22 @@
+describe("Links em nova guia/janela", () => {
+  it("Validando o atributo da link do instagram", () => {
+    cy.start();
+    cy.submitLogin("papito@webdojo.com", "katana123");
+
+    cy.get('[data-cy="instagram-link"]')
+      .should("have.attr", "href", "https://www.instagram.com/qapapito")
+      .and("have.attr", "target", "_blank");
+  });
+
+  //Acessando o link sem abrir uma nova guia/janela
+  it.only("Acessando o link do instagram sem abrir nova guia/janela", () => {
+    cy.start();
+    cy.submitLogin("papito@webdojo.com", "katana123");
+
+    cy.contains("Formulários").click();
+
+    cy.contains("a", "termos de uso").invoke("removeAttr", "target").click();
+
+    cy.contains("Ao acessar e usar nossos").should("be.visible");
+  });
+});

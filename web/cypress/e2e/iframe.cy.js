3c169f683969f0779b85258f70b002c678fca03f
Juliana Matos <jumatosk@gmail.com>
2025-10-22 10:38:36 -0300

    feat: lidando com iframe

---
+++ web/cypress/e2e/iframe.cy.js
@@ -0,0 +1,15 @@
+describe("iFrame", () => {
+  it("Deve tocar o vídeo corretamente", () => {
+    cy.start();
+    cy.submitLogin(Cypress.env("USER_EMAIL"), Cypress.env("USER_PASSWORD"));
+    cy.contains("Video").click();
+
+    cy.get("iframe[title='Video Player']")
+      .should("exist")
+      .its("0.contentDocument.body")
+      .then(cy.wrap)
+      .as("iFramePlayer");
+
+    cy.get("@iFramePlayer").find("button").click();
+  });
+});
